a6b03d24356b2cedffb53f4ecf9adfbed256aff1
vas1levnv <vas1levnv@yandex.ru>
2023-07-31 14:52:47 +0300

    деплой

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './App.scss';
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import MainContent from "./components/MainContent/MainContent";
-import TimerWrapper from "./components/Timer/TimerWrapper";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <main>
-                <TimerWrapper/>
                 <MainContent/>
             </main>
             <Footer/>
